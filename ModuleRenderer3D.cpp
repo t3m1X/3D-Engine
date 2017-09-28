6ab8b911d4b32ee33d80e42093d9f890b0c39006
rogerbusquets97 <duranbusquetsroger@gmail.com>
2017-09-28 12:02:51 +0200

    Glew implemented
    Added and initialized glew library

--- ModuleRenderer3D.cpp
+++ ModuleRenderer3D.cpp
@@ -1,12 +1,12 @@
-#include "Globals.h"
+
 #include "Application.h"
 #include "ModuleRenderer3D.h"
-#include "SDL\include\SDL_opengl.h"
-#include <gl/GL.h>
-#include <gl/GLU.h>
+
+
 
 #pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
+#pragma comment (lib, "glew/lib/Release/x64/glew32s.lib")
 
 ModuleRenderer3D::ModuleRenderer3D(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -74,7 +74,18 @@ bool ModuleRenderer3D::Init()
 			LOG("Error initializing OpenGL! %s\n", gluErrorString(error));
 			ret = false;
 		}
-		
+
+		GLenum err = glewInit();
+		if (err != GLEW_OK) {
+			LOG("GLEW NOT OK");
+			exit(EXIT_FAILURE);
+		}
+		LOG("Using Glew %s", glewGetString(GLEW_VERSION));
+		LOG("Vendor: %s", glGetString(GL_VENDOR));
+		LOG("Renderer: %s", glGetString(GL_RENDERER));
+		LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+		LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
 		GLfloat LightModelAmbient[] = {0.6f, 0.6f, 0.6f, 1.0f};
 		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, LightModelAmbient);
 		
--- ModuleRenderer3D.h
+++ ModuleRenderer3D.h
@@ -4,6 +4,11 @@
 #include "glmath.h"
 #include "Light.h"
 
+#include "glew\include\GL\glew.h"
+#include "SDL\include\SDL_opengl.h"
+#include <gl\GL.h>
+#include <gl\GLU.h>
+
 #define MAX_LIGHTS 8
 
 class ModuleRenderer3D : public Module
