7dfd9b2b539009d86f130b68665257cc093a4bb0
Sergi PR <t3m1X@users.noreply.github.com>
2017-10-10 14:09:35 +0200

    Saved vertices and indices in VRAM

--- ModuleLoader.cpp
+++ ModuleLoader.cpp
@@ -3,6 +3,7 @@
 #include "Assimp/include/scene.h"
 #include "Assimp/include/postprocess.h"
 #include "Assimp/include/cfileio.h"
+#include "glew\include\GL\glew.h"
 
 
 #pragma comment (lib, "Assimp/libx86/assimp.lib")
@@ -11,6 +12,11 @@
 bool ModuleLoader::CleanUp()
 {
 	aiDetachAllLogStreams();
+	while (!meshes.empty())
+	{
+		delete[] meshes.begin;
+		meshes.pop_front;
+	}
 	return true;
 }
 
@@ -27,16 +33,21 @@ void ModuleLoader::LoadFBX(char* path)
 		for ( int i = 0; i < scene->mNumMeshes; i++) {
 			//Vertices
 			aiMesh* m = scene->mMeshes[i];
-			Mesh new_mesh;
-			new_mesh.num_vertices = m->mNumVertices;
-			new_mesh.vertices = new float[new_mesh.num_vertices * 3];
-			memcpy(new_mesh.vertices, m->mVertices, sizeof(float) * new_mesh.num_vertices * 3);
-			LOG("New mesh with %d vertices", new_mesh.num_vertices);
+			Mesh* new_mesh = new Mesh;
+			new_mesh->num_vertices = m->mNumVertices;
+			new_mesh->vertices = new float[new_mesh->num_vertices * 3];
+			memcpy(new_mesh->vertices, m->mVertices, sizeof(float) * new_mesh->num_vertices * 3);
+			LOG("New mesh with %d vertices", new_mesh->num_vertices);
+			glGenBuffers(1, (GLuint*) &(new_mesh->id_vertices));
+			glBindBuffer(GL_ARRAY_BUFFER, new_mesh->id_vertices);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * new_mesh->num_vertices * 3, new_mesh->vertices, GL_STATIC_DRAW);
+
+
 			//Indices
 
 			if (m->HasFaces()) {
-				new_mesh.num_indices = m->mNumFaces * 3;
-				new_mesh.indices = new uint[new_mesh.num_indices];
+				new_mesh->num_indices = m->mNumFaces * 3;
+				new_mesh->indices = new uint[new_mesh->num_indices];
 
 				for (uint i = 0; i < m->mNumFaces; ++i)
 				{
@@ -44,13 +55,17 @@ void ModuleLoader::LoadFBX(char* path)
 						LOG("WARNING, geometry face with != 3 indices!");
 					}
 					else
-						memcpy(&new_mesh.indices[i * 3], m->mFaces[i].mIndices, 3 * sizeof(uint));
+						memcpy(&new_mesh->indices[i * 3], m->mFaces[i].mIndices, 3 * sizeof(uint));
 				}
 
 			}
+			glGenBuffers(1, (GLuint*) &(new_mesh->id_indices));
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, new_mesh->id_indices);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * new_mesh->num_indices, new_mesh->indices, GL_STATIC_DRAW);
 
-			meshes.push_back(&new_mesh);
-			
+
+			meshes.push_back(new_mesh);
+			glBindBuffer(NULL, 0);
 		}
 
 		aiReleaseImport(scene);
