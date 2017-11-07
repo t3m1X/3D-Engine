36b7351c56eee56e30b70f8508aa67a2e1d3f0f6
Sergi PR <t3m1X@users.noreply.github.com>
2017-11-07 19:33:14 +0100

    Matrices do not multiply to avoid MathGeoLib crash, temporal fix

--- Transform.cpp
+++ Transform.cpp
@@ -145,9 +145,9 @@ void Transform::RecalculateTransform()
 	else
 		global_transform.Set(local_transform);
 
-	if (old_global.IsInvertible())
-		owner->RecalculateAABB(old_global.Inverted().Mul(global_transform)); //Calculating the inversed sometimes causes the program to crash
-	else
+	//if (old_global.IsInvertible())
+	//	owner->RecalculateAABB(old_global.Inverted().Mul(global_transform)); //Calculating the inversed sometimes causes the program to crash
+	//else
 		owner->RecalculateAABB();
 
 	vector<GameObject*> childs = owner->GetChild();
